19bf53c1fad927fb4619b416ffd9e43d0e5f8bd8
parraAnthony <parraanthony359@gmail.com>
2023-09-02 20:53:54 -0400

    fase 4

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,20 @@ function App() {
   },[])
   const getAllUsers =()=>{
      axios
-      .get("https://users-crud.academlo.tech/users/")
+      .get("https://users-crud-so5y.onrender.com/users")
       .then(resp=>setUsers(resp.data))
       .catch(error=>console.error(error))
   }
   const newUser=(data)=>{ 
     if (fillForm){
       axios
-        .put(`https://users-crud.academlo.tech/users/${fillForm.id}/`, data )
+        .put(`https://users-crud-so5y.onrender.com/users/${fillForm.id}`, data )
         .then(()=>setFillForm(null),getAllUsers())
         .catch(error=>console.error(error))
+        .finally(()=>getAllUsers())
     }else{
       axios
-        .post('https://users-crud.academlo.tech/users/',data)
+        .post('https://users-crud-so5y.onrender.com/users',data)
         .then(()=>getAllUsers())
         .catch(error=>console.error(error)) 
     }
@@ -32,7 +33,7 @@ function App() {
   }
   const deleteUser =(data)=>{
     axios
-      .delete(`https://users-crud.academlo.tech/users/${data.id}/`)
+      .delete(`https://users-crud-so5y.onrender.com/users/${data.id}`)
       .then(()=>getAllUsers())
       .catch(error=>console.error(error))
   }
